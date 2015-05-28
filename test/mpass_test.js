2059065a33178386e03e0e0990d5fd64967f18f0
Jason Mulligan <jason.mulligan@avoidwork.com>
2015-05-28 18:52:11 -0400

    Adding an extreme test case

--- test/mpass_test.js
+++ test/mpass_test.js
@@ -39,5 +39,32 @@ exports["mpass"] = {
 
 		test.equal( x.indexOf('undefined'), -1, "Should be '-1'");
 		test.done();
+	},
+	extreme: function (test) {
+		var x = mpass(1, true ),
+			subFound = false,
+			extraFound = false;
+
+		test.expect(6);
+		test.equal(x.length > 0, true, "Should be 'true'");
+		test.equal(/[A-Z]/.test(x), true, "Should be 'true'");
+		test.equal(/[0-9]/.test(x), true, "Should be 'true'");
+
+		subs.forEach(function ( i ) {
+			if ( !subFound && x.indexOf( i ) > -1 ) {
+				subFound = true;
+				test.equal(true, true, "Should be 'true'");
+			}
+		});
+
+		extra.forEach(function ( i ) {
+			if ( !extraFound && x.indexOf( i ) > -1 ) {
+				extraFound = true;
+				test.equal(true, true, "Should be 'true'");
+			}
+		});
+
+		test.equal( x.indexOf('undefined'), -1, "Should be '-1'");
+		test.done();
 	}
 };
